2d99c05fc355ac21ca1a58ea4086698ea6e95bf1
Dave Olsthoorn <dave@bewaar.me>
2020-08-27 22:30:43 +0200

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,88 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as glob from '@actions/glob'
+import * as github from '@actions/github'
+import * as convert from 'xml-js'
+import * as fs from 'fs'
+import * as path from 'path'
+
+type Annotation = {
+  path: string
+  start_line: number
+  end_line: number
+  start_column: number
+  annotation_level: 'warning' | 'failure' | 'notice'
+  message: string
+  title: string
+  raw_details: string
+}
+
+const octokit = github.getOctokit(core.getInput('token', {required: true}))
+
+function submitAnnotations(annotations: Annotation[]): void {
+  for (let s = 0; s < annotations.length / 50; s++) {
+    octokit.checks.update({
+      ...github.context.repo,
+      check_run_id: github.context.runId,
+      output: {
+        summary: 'Android linting',
+        annotations: annotations.splice(s * 50, (s + 1) * 50)
+      }
+    })
+  }
+}
+
+function stripFilePath(filePath: string): string | null {
+  const filePathParts = filePath.split(path.sep)
+
+  const repoPath = path.resolve('.')
+  const repoPathParts = repoPath.split(path.sep)
+
+  // eslint-disable-next-line @typescript-eslint/no-for-in-array
+  for (const i in repoPathParts) {
+    if (repoPathParts[i] !== filePathParts[i]) return null
+  }
+
+  return filePathParts.slice(repoPathParts.length).join(path.sep)
+}
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+  const globber = await glob.create('**/build/reports/lint-results.xml')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  for await (const file of globber.globGenerator()) {
+    core.debug(`creating annotations from ${file}`)
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    const report = fs.readFileSync(file, 'utf8')
+    const xml = convert.xml2js(report)
+    const annotations: Annotation[] = []
+
+    for (const issueElement of xml.elements[0].elements) {
+      // Skip if we somehow encounter a different element then issue
+      if (issueElement.name !== 'issue') continue
+
+      for (const locationElement of issueElement.elements) {
+        // Skip if we somehow got a different element
+        if (locationElement.name !== 'location') continue
+
+        // get the file lication in the repository
+        const repoFilePath = stripFilePath(locationElement.attributes['file'])
+
+        /// Skip if the location was not in the repository
+        if (repoFilePath === null) continue
+
+        annotations.push({
+          path: repoFilePath,
+          start_line: parseInt(locationElement.attributes['line'], 10),
+          end_line: parseInt(locationElement.attributes['line'], 10),
+          start_column: parseInt(locationElement.attributes['column'], 10),
+          annotation_level: issueElement.attributes['severity'],
+          message: issueElement.attributes['message'],
+          title: `${issueElement.attributes['category']} - ${issueElement.attributes['summary']}`,
+          raw_details: issueElement.attributes['explanation']
+        })
+      }
+    }
+
+    submitAnnotations(annotations)
   }
 }
 
